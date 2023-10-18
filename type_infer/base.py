8dedb636c636138cbe86b09e9ce9b37ff7fe4bd2
Pedro Fluxa <pedro@mindsdb.com>
2023-10-18 08:47:08 +0000

    fixed a few typos in docs

--- type_infer/base.py
+++ type_infer/base.py
@@ -7,13 +7,20 @@ from dataclasses_json import dataclass_json
 @dataclass
 class TypeInformation:
     """
-    For a dataset, provides information on columns types, how they're used, and any other potential identifiers.
+    For a dataset, provides information on columns types.
 
-    ``TypeInformation`` is generated within :py:func:`infer.infer_types`, where small samples of each column are evaluated in a custom framework to understand what kind of data type the model is. The user may override data types, but it is recommended to do so within a JSON-AI config file.
+    ``TypeInformation`` is generated within :py:func:`infer.infer_types`,
+    where a small subset of samples of each column are evaluated in a custom
+    framework to understand what kind of data type the model is. The user
+    may override data types, but it is recommended to do so within a JSON-AI
+    config file.
 
     :param dtypes: For each column's name, the associated data type inferred.
-    :param additional_info: Any possible sub-categories or additional descriptive information.
-    :param identifiers: Columns within the dataset highly suspected of being identifiers or IDs. These do not contain informatic value, therefore will be ignored in subsequent training/analysis procedures unless manually indicated.
+    :param additional_info: Any possible sub-categories or additional descriptive
+           information.
+    :param identifiers: Columns within the dataset highly suspected of being identifiers
+           or IDs. These do not contain useful information, and should therefore be
+           ignored in subsequent training/analysis procedures unless manually indicated.
     """ # noqa
 
     dtypes: Dict[str, str]
